f012498c03b9f21663ea490eb61c02e8108cdd3d
samgozman <sam@gozman.space>
2021-02-13 21:03:39 +0300

    Рефакторинг

--- index.js
+++ index.js
@@ -1,13 +1,90 @@
+const cheerio = require('cheerio')
 const {
-    getAllStocks,
-    getAllEtfs,
-    getAllBonds,
-    getAllCurrencies
-} = require('./lib/tinkoff')
+    getAvailable
+} = require('./lib/utils')
 
-const main = async () => {
-    const result = await getAllEtfs()
-    console.log(result)
+/**
+ * @typedef {{quote: string, name: string}} quote
+ */
+
+ /**
+ * Возвращает все торгующиеся акции в тинькофф инвестициях
+ * 
+ * @async
+ * @return {Promise.<Array.<quote>>}
+ */
+const getTinkoffStocks = async () => {
+    try {
+        return await getAvailable('https://www.tinkoff.ru/invest/stocks')
+    } catch (error) {
+        return {
+            error: error.message
+        }
+    }
 }
 
-main()
+/**
+ * Возвращает все торгующиеся фонды в тинькофф инвестициях
+ * 
+ * @async
+ * @return {Promise.<Array.<quote>>}
+ */
+const getTinkoffEtfs = async () => {
+    try {
+        return await getAvailable('https://www.tinkoff.ru/invest/etfs')
+    } catch (error) {
+        return {
+            error: error.message
+        }
+    }
+}
+
+/**
+ * Возвращает все торгующиеся облигации в тинькофф инвестициях
+ * 
+ * @async
+ * @return {Promise.<Array.<quote>>}
+ */
+const getTinkoffBonds = async () => {
+    try {
+        return await getAvailable('https://www.tinkoff.ru/invest/bonds')
+    } catch (error) {
+        return {
+            error: error.message
+        }
+    }
+}
+
+/**
+ * Возвращает все торгующиеся валютные пары в тинькофф инвестициях
+ * 
+ * @async
+ * @return {Promise.<Array.<quote>>}
+ */
+const getTinkoffCurrencies = async () => {
+    try {
+        return await getAvailable('https://www.tinkoff.ru/invest/currencies')
+    } catch (error) {
+        return {
+            error: error.message
+        }
+    }
+}
+/**
+ * Возвращает все торгующиеся инструменты в тинькофф инвестициях (акции, облигации, фонды, валютные пары)
+ * ! Функция не быстрая, тк асинхронно работает 4 парсера.
+ * 
+ * @async
+ * @return {Promise.<Array.<quote>>}
+ */
+const getTinkoffQuotes = async () => {
+    return Array.prototype.concat(await getTinkoffCurrencies(), await getTinkoffBonds(), await getTinkoffEtfs(), await getTinkoffStocks())
+}
+
+module.exports = {
+    getTinkoffStocks,
+    getTinkoffEtfs,
+    getTinkoffBonds,
+    getTinkoffCurrencies,
+    getTinkoffQuotes
+}
